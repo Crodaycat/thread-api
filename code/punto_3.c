27414be683d67b3bc5d7d47129614bce0f744f8c
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-04-19 22:33:59 -0500

    FEAT: Código punto 3

---
+++ code/punto_3.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#define MAXLON 1000
+
+void *cuenta (void *);
+
+int main (int argc, char *argv[]) { 
+  if (argc!= 2) {
+    printf ("Indica el nombre de un fichero.\n");
+    exit(0);
+  }
+
+  pthread_t tid;
+
+  pthread_create(&tid, NULL, cuenta, argv[1]);
+
+  pthread_join(tid, NULL);
+
+  return 0;
+}
+
+void *cuenta (void *fileName) {
+  char *nombre = (char *) fileName;
+  int pos, cont= 0, leidos;
+  char cadena[MAXLON];
+  int fd;
+  fd= open(nombre,O_RDONLY);
+  while ((leidos =read(fd,cadena,MAXLON))!= 0) {
+    for (pos= 0; pos< leidos; pos++) {
+      if ((cadena[pos]== 'a') || (cadena[pos]== 'A')) {
+        cont++;
+      }
+    }
+  }
+  printf("Fichero %s: %d caracteres 'a' o 'A' encontrados\n", nombre, cont);
+  close(fd);
+
+  return (void *) NULL;
+}
